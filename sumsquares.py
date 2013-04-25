3adf56f92c8654f49392b229e588c1856113dc4a
jessicajunk <jajunk@uw.edu>
2013-04-24 23:18:11 -0700

    added comment

--- sumsquares.py
+++ sumsquares.py
@@ -1,3 +1,4 @@
+#This computes the sum of the squares of the positive integers up to n, assuming that n is at most 10000.
 %cython
 def sum_squares(long n):
     return n * (n + 1) * (2 * n + 1) / 6
